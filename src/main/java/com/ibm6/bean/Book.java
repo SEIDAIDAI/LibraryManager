2d4f5db7d10586896f3e0f67196dbb9a18981f2a
Linda <zds_candy@163.com>
2020-11-16 21:19:46 +0800

    增加了全部mapper的xml文件

---
+++ src/main/java/com/ibm6/bean/Book.java
@@ -0,0 +1,75 @@
+package com.ibm6.bean;
+
+public class Book {
+	private int bookId;
+	private String bookName;
+	private String nation;
+	private String type;
+	private String length;
+	private String theme;
+	private String storeDate;
+	private int leftAmount;
+	private int uploadAmount;
+	private int downloadAmount;
+	public int getBookId() {
+		return bookId;
+	}
+	public void setBookId(int bookId) {
+		this.bookId = bookId;
+	}
+	public String getBookName() {
+		return bookName;
+	}
+	public void setBookName(String bookName) {
+		this.bookName = bookName;
+	}
+	public String getNation() {
+		return nation;
+	}
+	public void setNation(String nation) {
+		this.nation = nation;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public String getLength() {
+		return length;
+	}
+	public void setLength(String length) {
+		this.length = length;
+	}
+	public String getTheme() {
+		return theme;
+	}
+	public void setTheme(String theme) {
+		this.theme = theme;
+	}
+	public String getStoreDate() {
+		return storeDate;
+	}
+	public void setStoreDate(String storeDate) {
+		this.storeDate = storeDate;
+	}
+	public int getLeftAmount() {
+		return leftAmount;
+	}
+	public void setLeftAmount(int leftAmount) {
+		this.leftAmount = leftAmount;
+	}
+	public int getUploadAmount() {
+		return uploadAmount;
+	}
+	public void setUploadAmount(int uploadAmount) {
+		this.uploadAmount = uploadAmount;
+	}
+	public int getDownloadAmount() {
+		return downloadAmount;
+	}
+	public void setDownloadAmount(int downloadAmount) {
+		this.downloadAmount = downloadAmount;
+	}
+	
+}
