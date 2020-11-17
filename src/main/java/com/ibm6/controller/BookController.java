d333537ce78dd5cc5af27255486bc7252f353181
Linda <zds_candy@163.com>
2020-11-17 11:49:02 +0800

    bookOK

--- src/main/java/com/ibm6/controller/BookController.java
+++ src/main/java/com/ibm6/controller/BookController.java
@@ -1,10 +1,14 @@
 package com.ibm6.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.ibm6.bean.Book;
 import com.ibm6.mapper.BookMapper;
 import com.ibm6.model.BookSearchType;
 import com.ibm6.service.BookService;
@@ -16,9 +20,25 @@ public class BookController {
 	@Autowired
 	private BookService bookService;
 	
-	@RequestMapping("/BookSearch")
-	public void BookSearchType(@RequestBody BookSearchType args)
+	@PostMapping("/BookType")
+	public List<Book> BookSearchType(@RequestBody Book args)
 	{
-		int re = bookService.bookSearchByType(args);
+		List<Book> re = bookService.bookSearchByType(args);
+		return re;
+	}
+	
+	@PostMapping("/BookInfo")
+	public Book BookInformation(@RequestBody Book args)
+	{
+		System.out.println(args.getBookId());
+		Book re = bookService.showBookInfo(args);
+		return re;
+	}
+	
+	@PostMapping("/BookKeyWord")
+	public List<Book> BookSearchKeyword(@RequestBody Book book)
+	{
+		List<Book> re = bookService.bookSearchByKeyword(book);
+		return re;
 	}
 }
