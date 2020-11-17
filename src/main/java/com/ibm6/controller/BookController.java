ae11c966a54e5f1e6a1c88344745da53ac979ee8
Linda <zds_candy@163.com>
2020-11-17 17:59:38 +0800

    1117

--- src/main/java/com/ibm6/controller/BookController.java
+++ src/main/java/com/ibm6/controller/BookController.java
@@ -3,12 +3,15 @@ package com.ibm6.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ibm6.bean.Book;
+import com.ibm6.bean.User;
 import com.ibm6.mapper.BookMapper;
 import com.ibm6.model.BookSearchType;
 import com.ibm6.service.BookService;
@@ -41,4 +44,30 @@ public class BookController {
 		List<Book> re = bookService.bookSearchByKeyword(book);
 		return re;
 	}
+	
+	@GetMapping("/BookByPage/{index}")
+	public List<Book> BookSearchPage(@PathVariable("index") Integer index)
+	{
+		List<Book> re = bookService.bookSearchByPage(index);
+		return re;
+	}
+	
+	@PostMapping("/BookInfoUpdate")
+	public int BookInfoUpdate(Book book)
+	{
+		System.out.println(book.getBookId());
+		System.out.println(book);
+		int re = bookService.bookInfoUpdate(book);
+		return re;
+	}
+	
+	/*
+	 * @GetMapping("/UserSearch/{index}") public List<User>
+	 * UserSearch(@PathVariable("index") Integer index) { List<User> re =
+	 * bookService.UserSearch(index); return re; }
+	 */
+	
+	
+	
+	
 }
--- src/main/java/com/ibm6/service/BookService.java
+++ src/main/java/com/ibm6/service/BookService.java
@@ -8,13 +8,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.ibm6.bean.Book;
+import com.ibm6.bean.User;
 import com.ibm6.mapper.BookMapper;
+import com.ibm6.mapper.UserMapper;
 import com.ibm6.model.BookSearchType;
 
 @Service
 public class BookService {
 	@Autowired
 	private BookMapper bookMapper;
+	private UserMapper userMapper;
 		
 	public List<Book> bookSearchByType(Book book) {
 		List<Book> bookList = bookMapper.findByCondition(book);
@@ -34,5 +37,26 @@ public class BookService {
 		return bookList;
 	}
 	
+	public List<Book> bookSearchByPage(Integer index)
+	{
+		List<Book> bookList = bookMapper.findByPage(index);
+
+		return bookList;
+	}
 	
+	public int bookInfoUpdate(Book book)
+	{
+		int re = bookMapper.updateById(book);
+
+		return re;
+	}
+	
+	/*
+	 * public List<User> UserSearch(int index) { List<User> re =
+	 * userMapper.selectById(index); }
+	 */
 }
+
+
+
+
